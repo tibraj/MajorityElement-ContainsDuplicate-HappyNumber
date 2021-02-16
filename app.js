7fde944bf6935385a20c323af92ba6d85ff1bef5
tibraj <tibraj94@gmail.com>
2021-02-16 13:23:25 -0500

    return array w/o duplicates and array containing duplicates

--- app.js
+++ app.js
@@ -7,5 +7,27 @@ function containsDuplicate(nums) {
     return nums.length !== new Set(nums).size;
 }
 
+function returnContainsDuplicate(nums) {
+    console.log(new Set(nums));
+    return new Set(nums);
+}
+
+function returnDuplicates(nums) {
+    let cache = {};
+    let results = [];
+    for(let i = 0; i < nums.length; i++) {
+        if(cache[nums[i]] === true) {
+            results.push(nums[i]);
+        } else {
+            cache[nums[i]] = true;
+        }
+    }
+    console.log(results);
+    return results;
+}
+
+let numbers = [1,2,3,1,2];
+returnContainsDuplicate(numbers);
+returnDuplicates(numbers);
 
 //Happy Number
